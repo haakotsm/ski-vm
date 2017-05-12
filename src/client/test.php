3dcc0fec51a178a2d606b226d0149e87b21bd85e
Duy Johnny Khac Nguyen <djkngul@hotmail.com>
2017-05-12 19:39:04 +0200

    athletes added (service,model etc.)

--- src/client/test.php
+++ src/client/test.php
@@ -7,6 +7,8 @@
 	 */
 
 	include 'app/resources/user-recources/userservice.php';
-	include 'app/resources/spectators-recources/spectatorservice.php';
+	include 'app/resources/athlete-resource/athleteservice.php';
 
-
+$AS = new AthleteService();
+echo $AS->getAthletes()[0]->fornavn;
+echo $AS->getAthleteForEvent(2)[0]->fornavn;
