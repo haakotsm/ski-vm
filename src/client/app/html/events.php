106f132f50d451c46c20acf40333d757c8500566
Duy Johnny Khac Nguyen <djkngul@hotmail.com>
2017-05-13 18:56:06 +0200

    ØVELSER FERDIG

--- src/client/app/html/events.php
+++ src/client/app/html/events.php
@@ -3,7 +3,7 @@
             $.get("app/resources/event-resources/methods/getEvents.php", function (resultat) {
             $.each(JSON.parse(resultat), function (index, element) {
                 var athleteArray = [];
-                $.ajax({url: "app/resources/athlete-resource/methods/getAthleteForEvent.php?eventId=" + index,
+                $.ajax({url: "app/resources/athlete-resource/methods/getAthleteForEvent.php?eventId=" + element.id,
                     type: 'get',
                     async: false,
                     success: function (resultat) {
@@ -13,22 +13,23 @@
                 }});
 
                 $('#eventContainer').append('' +
-                    '<div class="card col-4 bg-info"> ' +
-                        '<div class="card-block p-t-2">' +
+                    '<div class="card m-2 col-5 pb-4 bg-info"> ' +
+                        '<div class="card-block p-2">' +
                             '<h2 class="justify-content-start">'+ element.navn +'</h2>' +
-                            '<b">Verdensrekord: </b><br>' + element.rekordholder + ' | ' + element.verdensrekord +
+                            '<b>Verdensrekord: </b><br>' + element.rekordholder + ' | ' + element.verdensrekord + '<br>' +
+                            '<a class="nav-link" href="app/html/spectators.php?event='+ element.id  +'"> Se tilskuere </a>' +
                         '</div>' +
-                        '<div class="card-block p-t-2 bg-faded">' +
+                        '<div class="card-block p-2 bg-faded">' +
                             '<h3 class="justify-content-start"> Deltagere: </h3>' +
                             '<div class="dropdown-divider"></div>' +
                                 "<div id='athletes" + (index) + "'>" +
                                 '</div>' +
                             '</div>' +
                         '</div>'
-                )
+                );
 
                 for (var i = 0; i < athleteArray.length; i++) {
-                    $('#athletes'+(index-1)).append(athleteArray[i].fornavn + ' ' + athleteArray[i].etternavn + '<br>');
+                    $('#athletes'+(index)).append(athleteArray[i].fornavn + ' ' + athleteArray[i].etternavn + '<br>');
                 }
             });
         });
@@ -37,7 +38,7 @@
 <table id="myTable" class="table table-sm table-hover">
 </table>
 <div id="events">
-    <section class="container p-t-3">
+    <section class="container">
         <div class="row">
             <div class="col-lg-12">
                 <h1>Øvelser</h1>
