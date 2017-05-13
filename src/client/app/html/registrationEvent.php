e7702668ff75a8fb68cf8c571bba6c822e39e091
AT <at_p92@hotmail.com>
2017-05-13 13:45:22 +0200

    administration pages added

---
+++ src/client/app/html/registrationEvent.php
@@ -0,0 +1,9 @@
+<div id="registrationevent" class="col-6 ml-auto mr-auto px-0" >
+    <h2>Legg til Øvelse</h2>
+    <div class="list-group">
+        <a href="#" class="list-group-item">Øvelse 1</a>
+        <a href="#" class="list-group-item">Øvelse 2</a>
+        <a href="#" class="list-group-item">Øvelse 3</a>
+        <button type="button" class="btn btn-primary btn-block">Legg til</button>
+    </div>
+</div>
